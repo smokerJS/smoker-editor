8cec509afb6d64bc5e4b699ee77843c2f4dd496d
Jung, Jinsol <smokerjs.dev@gmail.com>
2019-12-24 13:27:59 +0900

    create container

---
+++ editor/container.js
@@ -0,0 +1,38 @@
+
+
+const basicFunction = [
+    {
+        key: 'bold',
+    },
+]
+
+
+const container = `
+    <div class="smoker_editor_container">
+        <div class="editor_header">
+            <ul>
+                <li>
+                    <i></i>
+                </li>
+                <li>
+                    <i></i>
+                </li>
+                <li>
+                    <i></i>
+                </li>
+                <li>
+                    <i></i>
+                </li>
+                <li>
+                    <i></i>
+                </li>
+            </ul>
+        </div>
+        <div class="editor_body">
+            <div contenteditable="true">
+
+            </div>
+        </div>
+    </div>
+`;
+
