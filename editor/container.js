26cd5e5002c462328840f948313245666bfc3f9f
Jung, Jinsol <smokerjs.dev@gmail.com>
2020-01-06 17:44:29 +0900

    update container heading tag

--- editor/container.js
+++ editor/container.js
@@ -24,12 +24,12 @@ const container = `
                     <button class="btn_editor_action btn_heading"><i>Heading</i></button>
                     <div class="layer_editor_action layer_heading">
                         <ul>
-                            <li><button><i>H1</i></button></li>
-                            <li><button><i>H2</i></button></li>
-                            <li><button><i>H3</i></button></li>
-                            <li><button><i>H4</i></button></li>
-                            <li><button><i>H5</i></button></li>
-                            <li><button><i>H6</i></button></li>
+                            <li><button><i data-replace-block="H1">H1</i></button></li>
+                            <li><button><i data-replace-block="H2">H2</i></button></li>
+                            <li><button><i data-replace-block="H3">H3</i></button></li>
+                            <li><button><i data-replace-block="H4">H4</i></button></li>
+                            <li><button><i data-replace-block="H5">H5</i></button></li>
+                            <li><button><i data-replace-block="H6">H6</i></button></li>
                         </ul>
                     </div>
                 </li>
