99a896556cd790e62f51b36e25f85db9fd017946
Jung, Jinsol <smokerjs.dev@gmail.com>
2020-01-08 18:41:00 +0900

    create SmokerEditorModule

--- editor/module/index.js
+++ editor/module/index.js
@@ -1,3 +1,46 @@
 class SmokerEditorModule {
-    
+    constructor({moduleName, buttonId, buttonClass, iconText, iconId, iconClass, iconImageURL}) {
+        this._moduleName = moduleName;
+        this._buttonId = buttonId;
+        this._buttonClass = buttonClass;
+        this._iconText = iconText;
+        this._iconId = iconId;
+        this._iconClass = iconClass;
+        this._iconImageURL = iconImageURL;
+    }
+
+    capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    /**
+     * @param {String} moduleName 
+     */
+    set moduleName(moduleName) {
+        if(moduleName) this._moduleName = this.capitalizeFirstLetter(String(moduleName));
+    }
+    get moduleName() {
+        return this._moduleName;
+    }
+
+    /**
+     * @param {String} buttonId 
+     */
+    set buttonId(buttonId) {
+        this._buttonId = String(buttonId);
+    }
+    get buttonId() {
+        return this._buttonId;
+    }
+
+
+
+    toggleHandler(moduleName) {
+        console.error(`SmokerEditor :: toggleHandler is not defined - ${moduleName}`);
+    }
 }
+
+const temp  = new SmokerEditorModule()
+temp.moduleName = '123'
+console.log(temp.moduleName)
+console.log(temp._moduleName)
