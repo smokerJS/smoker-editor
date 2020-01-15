514453f6951d664009bb11e845a24fea0c923301
Jung, Jinsol <smokerjs.dev@gmail.com>
2020-01-15 17:28:16 +0900

    create se module d.ts

---
+++ src/smoker-editor/types/SE_ModuleBase.d.ts
@@ -0,0 +1,10 @@
+interface SE_ModuleBase {
+    moduleName   : string;
+    entryHandler : Function;
+    buttonId?    : string;
+    buttonClass? : string;
+    iconId?      : string;
+    iconClass?   : string;
+    iconText?    : string;
+    iconImageURL?: string;
+}
